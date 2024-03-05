f9fb7051a59c4ff3d5eb9270275521c94988b81f
lasse <77295879+lassejlv@users.noreply.github.com>
2024-03-05 13:48:36 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Greenhole",
+  description: "holy moly it's a website",
 };
 
 const poppins_sans = Poppins({ subsets: ["latin"], weight: "400" });
