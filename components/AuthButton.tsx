b3cdbeb90e6cf7e2df3fecab4f7ee20ed81bd1a9
lasse <77295879+lassejlv@users.noreply.github.com>
2024-03-06 14:48:12 +0100

    Update AuthButton.tsx

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -17,12 +17,23 @@ export default async function AuthButton() {
     return redirect("/login");
   };
 
+  const account = async () => {
+    "use server";
+
+    return redirect("/account");
+  };
+
   return user ? (
     <div className="flex items-center gap-4">
       Hey, {user.email}!
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Logout
+        </button>
+      </form>
+      <form action={account}>
+        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+          Go to Account
         </button>
       </form>
     </div>
