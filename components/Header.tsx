8babc7224486e529700ae39bb78491484007c952
lasse <77295879+lassejlv@users.noreply.github.com>
2024-03-13 13:58:56 +0100

    changes lol

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,45 @@
 "use client";
 
-import React from 'react';
-import { NextPage } from 'next';
-import Menu from './menu/menu';
-import './header/style.css';
+import React from "react";
+import { NextPage } from "next";
+import Menu from "./menu/menu";
+import "./header/style.css";
 import { useRouter } from "next/navigation";
-const Header: NextPage = () => {
+import { createClient } from "@/utils/supabase/client";
+
+export default function Header() {
+  const [isLoggedIn, setIsLoggedIn] = React.useState<boolean>(false);
+
   const router = useRouter();
 
+  React.useEffect(() => {
+    async function check() {
+      const supabase = createClient();
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+
+      if (user) {
+        setIsLoggedIn(true);
+        console.log("User is logged in");
+      }
+    }
+
+    check();
+  }, []);
+
   return (
-
     <header className="p-4 flex items-center justify-between border-b-2 border-gray-400">
-    <div className="flex items-center space-x-64 header-body">
-      <img src="/icons/logo.png" alt="Logo" className="h-10 w-10 header-img" onClick={() => router.push("/")} />
+      <div className="flex items-center space-x-64 header-body">
+        <img
+          src="/icons/logo.png"
+          alt="Logo"
+          className="h-10 w-10 header-img"
+          onClick={() => router.push("/")}
+        />
 
-      <Menu />
+        <Menu isLoggedIn={isLoggedIn} />
       </div>
     </header>
   );
-};
-
-export default Header;
+}
