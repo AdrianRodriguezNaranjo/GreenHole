8f59d349334780ca33da076251589f8275701d7c
MiguelAngelHGProjects <miguelangelhernandezgarcia@alumno.ieselrincon.es>
2024-03-13 12:04:58 +0000

    changes for issue#8

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+<<<<<<< HEAD
 import React from "react";
 import { NextPage } from "next";
 import { MenuOutlined } from "@ant-design/icons";
@@ -7,10 +8,17 @@ import { Button, SearchBar, Space } from "antd-mobile";
 import Menu from "./menu/menu";
 import { useRouter } from "next/navigation";
 
+=======
+import React from 'react';
+import { NextPage } from 'next';
+import Menu from './menu/menu';
+import './header/style.css';
+>>>>>>> 0c6ad1d (changes for issue#8)
 const Header: NextPage = () => {
   const router = useRouter();
 
   return (
+<<<<<<< HEAD
     <header className="p-2 flex items-center justify-between border-b-2 border-gray-400 px-5">
       <img
         src="/icons/logo.png"
@@ -18,6 +26,11 @@ const Header: NextPage = () => {
         className="h-10 w-10"
         onClick={() => router.push("/")}
       />
+=======
+    <header className="p-4 flex items-center justify-between border-b-2 border-gray-400">
+    <div className="flex items-center space-x-64 header-body">
+      <img src="/icons/logo.png" alt="Logo" className="h-10 w-10 header-img" />
+>>>>>>> 0c6ad1d (changes for issue#8)
       <Menu />
     </header>
   );
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,26 +3,28 @@
 import React from 'react';
 import { NextPage } from 'next';
 import './styles.css'
-import { CopyrightCircleOutlined, CopyrightTwoTone, FacebookFilled, FacebookOutlined, GithubFilled, GithubOutlined, LinkedinFilled, LinkedinOutlined } from '@ant-design/icons';
+import { CopyrightCircleOutlined, GithubFilled } from '@ant-design/icons';
+
 const Footer: NextPage = () => {
-  return (
-    <div>
-        <div className='footer-body' style={{color:'white'}}>
-            <div className='footer-options'>
-                <a>Admin login</a>
-                <a href='https://github.com/AdrianRodriguezNaranjo/GreenHole/tree/develop' target='_blank'>About Us</a>
-                <a href='https://sdgs.un.org/es/goals' target='_blank'>UN goals</a>
-            </div>
-            <div style={{backgroundColor:'#658E9C'}} className='footer-spacing'></div>
-            <div className='footer-data'>
-                <p>Copyright <CopyrightCircleOutlined/> 2024<br/> All rights reserved.</p>
-                <div className='footer-icons'>
-                    <a href='https://github.com/AdrianRodriguezNaranjo/GreenHole/tree/develop' target='_blank'><GithubFilled style={{ fontSize: '30px', margin:'10px'}}/></a>
+    return (
+        <div>
+            <div className='footer-body' style={{ color: 'white' }}>
+                <div className='footer-options'>
+                    <a>Admin login</a>
+                    <a href='https://github.com/AdrianRodriguezNaranjo/GreenHole/tree/develop' target='_blank'>About Us</a>
+                    <a href='https://sdgs.un.org/es/goals' target='_blank'>UN goals</a>
+                </div>
+                <div style={{ backgroundColor: '#658E9C' }} className='footer-spacing'></div>
+                <div className='footer-data'>
+                    <p>Copyright <CopyrightCircleOutlined /> 2024<br /> All rights reserved.</p>
+                    <div className='footer-icons flex'>
+                        <a href='https://www.figma.com/file/GnBh4hvgNc5bjtQVf7jKpH/Kalm-project?type=design&node-id=91%3A2244&mode=design&t=bDtmQJ7NGbj2mB46-1' target='_blank'><img src='/icons/figma.svg' style={{ fontSize: '30px', margin: '10px' }} /></a>
+                        <a href='https://github.com/AdrianRodriguezNaranjo/GreenHole/tree/develop' target='_blank'><GithubFilled style={{ fontSize: '30px', margin: '10px' }} /></a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default Footer;
