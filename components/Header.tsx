17fd845b791df27982820d6dfb8b970c5ed2b0bb
lasse <77295879+lassejlv@users.noreply.github.com>
2024-03-13 10:13:22 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,25 @@
 "use client";
 
-import React from 'react';
-import { NextPage } from 'next';
-import Menu from './menu/menu';
-import './header/style.css';
+import React from "react";
+import { NextPage } from "next";
+import { MenuOutlined } from "@ant-design/icons";
+import { Button, SearchBar, Space } from "antd-mobile";
+import Menu from "./menu/menu";
+import { useRouter } from "next/navigation";
+
 const Header: NextPage = () => {
+  const router = useRouter();
+
   return (
-    <header className="p-4 flex items-center justify-between border-b-2 border-gray-400">
-    <div className="flex items-center space-x-64 header-body">
-      <img src="/icons/logo.png" alt="Logo" className="h-10 w-10 header-img" />
+    <header className="p-2 flex items-center justify-between border-b-2 border-gray-400 px-5">
+      <img
+        src="/icons/logo.png"
+        alt="Logo"
+        className="h-10 w-10"
+        onClick={() => router.push("/")}
+      />
       <Menu />
-    </div>      
-  </header>
-  
+    </header>
   );
 };
 
