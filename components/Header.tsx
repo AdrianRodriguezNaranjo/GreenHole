45309dba20e9389bfb5222c71196c7884c7462f1
lasse <77295879+lassejlv@users.noreply.github.com>
2024-03-13 10:09:58 +0100

    Small changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ import Menu from './menu/menu';
 
 const Header: NextPage = () => {
   return (
-    <header className="p-4 flex items-center justify-between border-b-2 border-gray-400">
+    <header className="p-2 flex items-center justify-between border-b-2 border-gray-400">
     <div className="flex items-center space-x-64">
       <img src="/icons/logo.png" alt="Logo" className="h-10 w-10" />
       <Menu />
